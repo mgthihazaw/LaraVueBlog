7a35eaa1de9e1d8f0e3f0adb43815cbc037e5180
mgthihazaw <mgthihazaw777@gmail.com>
2019-03-18 16:54:26 +0630

    position fixed sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,23 +1,45 @@
-<aside>
-	<header>
-		<h1><a>Microstack</a></h1>
-		<h2>Karaoke Software Comapny Limited</h2>
-	</header>
-	<nav>
-		<a href="/" class="nav-item">
-			Articles<span class="nav-description">automatically add the token to all request headers</span>
-		</a>
-		<a href="/" class="nav-item">
-			Invoices<span class="nav-description">automatically add the token to all request headers</span>
-		</a>
-        <a href="/" class="nav-item">
-			Bleik<span class="nav-description">automatically add the token to all request headers</span>
-		</a>
+<aside >
+	<div class="sidebar-content fixed-top">
+		<div class="fixed-top">
+			<header>
+				<h1><a>Microstack</a></h1>
+				<h2>Karaoke Software Comapny Limited</h2>
+			</header>
+			<nav>
+				<a href="/" class="nav-item">
+					Articles<span class="nav-description">automatically add the token to all request headers</span>
+				</a>
+				<a href="/" class="nav-item">
+					Invoices<span class="nav-description">automatically add the token to all request headers</span>
+				</a>
+		        <a href="/" class="nav-item">
+					Bleik<span class="nav-description">automatically add the token to all request headers</span>
+				</a>
 
-	</nav>
-	<div class="social-nav">
-	   <a href="https://facebook.com/" target="_blank">@include('svg.facebook')</a>
-	    <a href="https://github.com/" target="_blank">@include('svg.github')</a>
-	     <a href="https://google.com/" target="_blank">@include('svg.google')</a>
+			</nav>
+			
+
+
+
+
+			<nav>
+				<a href="/" class="nav-item">
+					Articles<span class="nav-description">automatically add the token to all request headers</span>
+				</a>
+				<a href="/" class="nav-item">
+					Invoices<span class="nav-description">automatically add the token to all request headers</span>
+				</a>
+		        <a href="/" class="nav-item">
+					Bleik<span class="nav-description">automatically add the token to all request headers</span>
+				</a>
+
+			</nav>
+			<div class="social-nav">
+			   <a href="https://facebook.com/" target="_blank">@include('svg.facebook')</a>
+			    <a href="https://github.com/" target="_blank">@include('svg.github')</a>
+			     <a href="https://google.com/" target="_blank">@include('svg.google')</a>
+			</div>
+			</div>
 	</div>
+
 </aside>
